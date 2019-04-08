4891224c51d90f90462a897e02f9aa4bf9215c25
Stanislav Lashmanov <stasvarenkin@gmail.com>
2019-04-08 21:18:19 +0300

    Print best value first, then anything else. Sort by best pixel value
    Also did a small refactor. Hope you don't mind.

--- unit-golf.js
+++ unit-golf.js
@@ -1,5 +1,7 @@
 const px = process.argv[2];
-const precision = process.argv[3];
+const precision = process.argv[3] || 3;
+
+const allowedOffset = 1;
 
 const conversions = [
   { unit: "px", value: 1 },
@@ -7,22 +9,30 @@ const conversions = [
   { unit: "mm", value: 3.765625 },
   { unit: "in", value: 96 },
   { unit: "pt", value: 1.328125 },
-  { unit: "pc", value: 16 },
   { unit: "em", value: 16 },
   { unit: "ex", value: 7.171875 },
   { unit: "vw", value: 4 },
   { unit: "vh", value: 3 },
-  { unit: "q", value: 0.94453125 }
+  { unit: "q",  value: 0.94453125 }
 ];
 
-const convert = px => {
+const findShortestValue = px => {
   return conversions
-    .map(({ unit, value }) => {
-      const converted = (px / value).toPrecision(precision || 3);
-      // casting back to number strips trailing 0s
-      return `${Number(converted)}${unit}`;
+    .map({ unit, value }) => {
+      const inUnits = (px / value).toPrecision(precision);
+      const string = `${Number(inUnits)}${unit}`;
+      const offset = Math.abs(inUnits * value - px);
+      return { string, offset };
     })
-    .sort((a, b) => a.length - b.length);
+    .sort((a, b) => {
+      const lnDiff = a.string.length - b.string.length;
+      if (a.offset >= allowedOffset || lnDiff > 0) return 1;
+      if (lnDiff < 0) return -1;
+      return a.offset - b.offset;
+    })
+    .map(res => res.string)
 };
 
-console.log(convert(px));
+const [best, ...rest] = findShortestValue(px);
+
+console.log(`Best match: ${best}, rest: ${rest}`);
