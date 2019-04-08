1396c94427d88cf78da548536597607bc0538ece
Stanislav Lashmanov <stasvarenkin@gmail.com>
2019-04-08 21:18:19 +0300

    Merge pull request #1 from CyberAP/patch-1
    Print best value first, then anything else. Sort by lowset pixel offset

--- unit-golf.js
+++ unit-golf.js
@@ -1,28 +1,68 @@
-const px = process.argv[2];
-const precision = process.argv[3];
+const px = Number(process.argv[2]);
+const allowedPixelOffset = Number(process.argv[3] || .5);
+const resultPrecision = Number(process.argv[4] || 2);
 
-const conversions = [
-  { unit: "px", value: 1 },
-  { unit: "cm", value: 37.78125 },
-  { unit: "mm", value: 3.765625 },
-  { unit: "in", value: 96 },
-  { unit: "pt", value: 1.328125 },
-  { unit: "pc", value: 16 },
-  { unit: "em", value: 16 },
-  { unit: "ex", value: 7.171875 },
-  { unit: "vw", value: 4 },
-  { unit: "vh", value: 3 },
-  { unit: "q", value: 0.94453125 }
+const units = [
+  { name: "px", multiplier: 1 },
+  { name: "cm", multiplier: 37.78125 },
+  { name: "mm", multiplier: 3.765625 },
+  { name: "in", multiplier: 96 },
+  { name: "pt", multiplier: 1.328125 },
+  { name: "em", multiplier: 16 },
+  { name: "ex", multiplier: 7.171875 },
+  { name: "vw", multiplier: 4 },
+  { name: "vh", multiplier: 3 },
+  { name: "q",  multiplier: 0.94453125 }
 ];
 
-const convert = px => {
-  return conversions
-    .map(({ unit, value }) => {
-      const converted = (px / value).toPrecision(precision || 3);
-      // casting back to number strips trailing 0s
-      return `${Number(converted)}${unit}`;
-    })
-    .sort((a, b) => a.length - b.length);
+const clampPrecision = (number, precision = 2) => {
+  const pow = Math.pow(10, precision);
+  return Number(Math.round(number * pow) / pow);
 };
 
-console.log(convert(px));
+const getUnitValues = (px, unit, unitValue) => {
+  const { name, multiplier } = unit;
+  unitValue = unitValue || clampPrecision(px / multiplier, resultPrecision);
+  const pixelOffset = clampPrecision(unitValue * multiplier - px);
+  return {
+    unitValue,
+    string: `${unitValue}${name}`.replace(/^0./, '.'),
+    pixelOffset,
+  };
+};
+
+const findBestUnitValue = px => unit => {
+  let result = getUnitValues(px, unit);
+  const { unitValue } = result;
+
+  if (!Number.isInteger(unitValue)) {
+    for (let i = unitValue.toString().split('.')[1].length-1; i >= 0; i--) {
+      const newUnitValue = clampPrecision(unitValue, i);
+      const newResult = getUnitValues(px, unit, newUnitValue);
+      const { pixelOffset } = newResult;
+      if (Math.abs(pixelOffset) <= allowedPixelOffset) {
+        result = newResult;
+      }
+    }
+  }
+
+  return result;
+};
+
+const convertAndSort = px => {
+  return units
+    .map(findBestUnitValue(px))
+    .sort((a, b) => {
+      const [lnA, lnB] = [a, b].map(item => item.string.length);
+      const [offsetA, offsetB] = [a.pixelOffset, b.pixelOffset].map(Math.abs);
+      const lnDiff = lnA - lnB;
+      if (offsetA > allowedPixelOffset) return 1;
+      if (lnDiff === 0) return offsetA - offsetB;
+      return lnDiff;
+    });
+};
+
+const [best, ...rest] = convertAndSort(px);
+
+console.log(`\nBest: ${best.string}, offset by: ${best.pixelOffset} pixels`);
+console.log(`\nRest:\n${rest.map(res => `${res.string} (offset: ${res.pixelOffset})`).join('\n')}`);
